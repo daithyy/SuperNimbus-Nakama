dd25d65d21ccabe5d533664396503e549a8dd9cf
Daithy <davidwbrennan@hotmail.com>
2021-12-09 14:50:53 +0000

    Implement roll up to project.

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,10 @@ WORKDIR /backend
 COPY package*.json ./
 RUN npm install
 
-COPY tsconfig.json ./
-COPY /src/*.ts ./src/
-RUN npx tsc
+COPY . .
+RUN npm run build
 
-FROM heroiclabs/nakama:3.8.0
+FROM heroiclabs/nakama:3.9.0
 
 COPY --from=node-builder /backend/build/*.js /nakama/data/modules/build/
 COPY local.yml /nakama/data/
--- build/index.js
+++ build/index.js
@@ -1,36 +1,41 @@
-"use strict";
-var InitModule = function (ctx, logger, nk, initializer) {
-    initializer.registerRpc("healthcheck", RpcHealthCheck);
-    initializer.registerRpc("servervalidate", ServerValidate);
-    logger.info("JavaScript module loaded");
-};
-var RpcHealthCheck = function (ctx, logger, nk, payload) {
-    logger.info("Healthcheck RPC called");
-    return JSON.stringify({ success: true });
-};
-var ServerValidate = function (ctx, logger, nk, payload) {
-    logger.info("payload %q", payload);
-    if (ctx.userId) {
-        // Reject non server-to-server call
-        throw Error("Cannot invoke this function from user session!");
-    }
-    var request = JSON.parse(payload);
-    var collectionName = "matchToken";
-    logger.info(request.userId);
-    logger.info(request.matchId);
-    var objectIds = [
-        {
-            collection: collectionName,
-            key: request.matchId,
-            userId: request.userId,
-        },
-    ];
-    var result = null;
-    try {
-        result = nk.storageRead(objectIds)[0];
-    }
-    catch (error) {
-        return error;
-    }
-    return JSON.stringify({ result: result });
-};
+function RpcHealthCheck(ctx, logger, nk, payload) {
+  logger.info("Healthcheck RPC called");
+  return JSON.stringify({
+    success: true
+  });
+}
+
+function ServerValidate(ctx, logger, nk, payload) {
+  logger.info("payload %q", payload);
+
+  if (ctx.userId) {
+    throw Error("Cannot invoke this function from user session!");
+  }
+
+  var request = JSON.parse(payload);
+  var collectionName = "matchToken";
+  var objectIds = [{
+    collection: collectionName,
+    key: request.matchId,
+    userId: request.userId
+  }];
+  var result = null;
+
+  try {
+    result = nk.storageRead(objectIds)[0];
+  } catch (error) {
+    throw Error(error);
+  }
+
+  return JSON.stringify({
+    result: result
+  });
+}
+
+function InitModule(ctx, logger, nk, initializer) {
+  initializer.registerRpc("healthcheck", RpcHealthCheck);
+  initializer.registerRpc("servervalidate", ServerValidate);
+  logger.info("JavaScript module loaded");
+}
+
+!InitModule && InitModule.bind(null);
